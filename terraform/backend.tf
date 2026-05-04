b45f25e049726986ba027e7095304d5d93d426a2
Guilherme de Oliveira Castro <guilhermegcastro20@gmail.com>
2026-05-04 00:52:20 -0300

    remove terraform folder

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-guicastro"
-    key     = "projetodevops/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
-
--- terraform/ecr.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ecr_repository" "ecr_next" {
-  name                 = "projeto_prod"
-  image_tag_mutability = "MUTABLE"
-
-}
-
